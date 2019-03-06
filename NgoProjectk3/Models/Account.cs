1ca527aeeed5f55a341cc34c81e914832a84bea1
nguyenhoang24695 <hoangnvd00536@fpt.edu.vn>
2019-03-06 14:47:59 +0700

    merge model UI into account

--- NgoProjectk3/Models/Account.cs
+++ NgoProjectk3/Models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,19 @@ namespace NgoProjectk3.Models
 {
     public class Account
     {
+        [Key]
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Salt { get; set; }
         public int Role { get; set; }
         public int Status { get; set; }
+        public string FullName { get; set; }
+        public int Gender { get; set; }
+        public string Address { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public DateTime DeletedAt { get; set; }
