912fe604327d02fabc6b133bf943bb98a4978d78
nguyenhoang24695 <hoangnvd00536@fpt.edu.vn>
2019-03-11 16:20:30 +0700

    create donation page for user

---
+++ NgoProjectk3/Controllers/DonationsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NgoProjectk3.DataContext;
+using NgoProjectk3.Models;
+
+namespace NgoProjectk3.Controllers
+{
+    public class DonationsController : Controller
+    {
+        private NgoProjectk3Context db = new NgoProjectk3Context();
+
+        // GET: Donations
+        public ActionResult Index()
+        {
+            return View(db.Donations.ToList());
+        }
+
+        // GET: Donations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donation);
+        }
+
+        // GET: Donations/Create
+        public ActionResult Create()
+        {
+            ViewBag.DonorId = new SelectList(db.Accounts, "Id", "FullName");
+            ViewBag.ProgramId = new SelectList(db.DonatePrograms, "Id", "Name");
+            return View();
+        }
+
+        // POST: Donations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,DonorId,ProgramId,Amount,Status,CreatedAt,UpdatedAt,DeletedAt")] Donation donation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donations.Add(donation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(donation);
+        }
+
+        // GET: Donations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donation);
+        }
+
+        // POST: Donations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,DonorId,ProgramId,Amount,Status,CreatedAt,UpdatedAt,DeletedAt")] Donation donation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(donation);
+        }
+
+        // GET: Donations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donation donation = db.Donations.Find(id);
+            if (donation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donation);
+        }
+
+        // POST: Donations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Donation donation = db.Donations.Find(id);
+            db.Donations.Remove(donation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
